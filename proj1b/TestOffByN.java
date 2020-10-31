d2299b939992f12d219c92f7171a77f3cf37d5fb
BellaXiao <286773696@qq.com>
2020-10-31 17:12:35 -0400

    Style check for TestOffByN

--- proj1b/TestOffByN.java
+++ proj1b/TestOffByN.java
@@ -4,13 +4,13 @@ import static org.junit.Assert.*;
 public class TestOffByN {
     static CharacterComparator offByN = new OffByN(2);
 
-   @Test
-    public void TestEqualChars() {
-        assertTrue(offByN.equalChars('a','c'));
-        assertTrue(offByN.equalChars('d','b'));
+    @Test
+    public void testEqualChars() {
+        assertTrue(offByN.equalChars('a', 'c'));
+        assertTrue(offByN.equalChars('d', 'b'));
 
-        assertFalse(offByN.equalChars('a','a'));
-        assertFalse(offByN.equalChars('D','b'));
-        assertFalse(offByN.equalChars('a','b'));
-   }
+        assertFalse(offByN.equalChars('a', 'a'));
+        assertFalse(offByN.equalChars('D', 'b'));
+        assertFalse(offByN.equalChars('a', 'b'));
+    }
 }
