a041a332ac399c825a475dc0de29ea4961446f5f
BellaXiao <286773696@qq.com>
2020-12-13 13:03:33 -0500

    lab11 Graph related

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+//import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.IndexMinPQ;
+
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +12,7 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private IndexMinPQ<Integer> pq;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,22 +21,45 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        pq = new IndexMinPQ<Integer>(m.V());
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return t - v;
     }
 
     /** Finds vertex estimated to be closest to target. */
-    private int findMinimumUnmarked() {
+    private int findMinimumUnmarked(int[] distTo) {
         return -1;
         /* You do not have to use this method. */
     }
 
     /** Performs an A star search from vertex s. */
-    private void astar(int s) {
-        // TODO
+    private void astar(int source) {
+        // TD
+        pq.insert(source, distTo[source] + h(source));
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            marked[v] = true;
+            announce();
+            if (v == t) {
+                return;
+            }
+            for (int w: maze.adj(v)) {
+                if (!marked[w]) {
+                    if (distTo[w] > (distTo[v] + 1)) {
+                        distTo[w] = distTo[v] + 1;
+                        edgeTo[w] = v;
+                        if (pq.contains(w)) {
+                            pq.decreaseKey(w, distTo[w] + h(w));
+                        } else {
+                            pq.insert(w, distTo[w] + h(w));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +12,95 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    int s = 0;
+    int[] parentTo;
+    boolean hasCycle = false;
+
 
     public MazeCycles(Maze m) {
         super(m);
+        parentTo = new int[maze.V()];
+        for (int i = 0; i < maze.V(); i += 1) {
+            parentTo[i] = Integer.MAX_VALUE;
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        // TD: Your code here!
+        // try recursive dfs first
+        dfs(s);
     }
 
+    /**
+     * Seems like use iterative version instead of recursion can solve below index problem.
+     * */
+    private void dfs(int r) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(r);
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            marked[v] = true;
+            for (int w: maze.adj(v)) {
+                if (marked[w] && parentTo[v] != w) {
+                    //copy cycle edges from parentTo to edgeTo, so that only them will be drawn.
+                    int pre = v;
+                    while (pre != w) {
+                        edgeTo[pre] = parentTo[pre];
+                        pre = parentTo[pre];
+                    }
+                    edgeTo[w] = v;
+                    announce();
+                    //hasCycle = true;
+                    return;
+                }
+                if (!marked[w]) {
+                    parentTo[w] = v;
+                    stack.push(w);
+                }
+            }
+        }
+    }
+
+
+    /*
+    still not quite sure why has IndexOutofBound Error for deleteOtherEdge func
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        if (hasCycle) {
+            return;
+        }
+        for (int w: maze.adj(v)) {
+            if (marked[w] && edgeTo[v] != w) {
+                // delete all edges from source to w, only left edges for cycle.
+                deleteOtherEdge(v, w);
+                edgeTo[w] = v;
+                announce();
+                hasCycle = true;
+                return;
+            }
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(w);
+            }
+        }
+    }
+
+    // delete edges from s to w
+    private void deleteOtherEdge(int v, int w) {
+        HashSet<Integer> cycleVertex = new HashSet<>();
+        int pre = v;
+        while (pre != w) {
+            cycleVertex.add(pre);
+            pre = edgeTo[pre];
+        }
+
+        for (int i = 0; i < edgeTo.length; i += 1) {
+            if (!cycleVertex.contains(i)) {
+                edgeTo[i] = Integer.MAX_VALUE;
+            }
+        }
+    }*/
 }
 
